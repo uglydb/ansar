749d40be5c0e6a8971ecd41586590fa18278c543
Dinislam <abubakir.dinislam@qomek.kz>
2024-07-02 12:29:06 +0500

    yes

---
+++ test/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoApi.Models;
+
+[Table("people")]
+public class TodoItem
+{
+    public long id { get; set; }
+    public string? name { get; set; }
+    public int age { get; set; }
+    public string? city { get; set; }
+}
